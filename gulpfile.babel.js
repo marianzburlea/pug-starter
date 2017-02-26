f556bc823e6217993ed3aa845cb48eb94b702ad8
Marian Zburlea <marianzburlea@gmail.com>
2017-02-26 23:37:16 +0000

    Add default task to gulp using ES6

---
+++ gulpfile.babel.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// GUlp is a simple platform-agnostic toolkit that helps you automate painful
+// and time-consuming tasks in your workflow
+import gulp from 'gulp';
+
+// Default gulp task
+gulp.task('default', () => {
+  console.log('Default gulp task');
+});
