aa0d53e48f50e6a8326bd14431a3565afb1577c7
Marian Zburlea <marianzburlea@gmail.com>
2017-02-27 14:35:00 +0000

    use wrench to load all gulp tasks and initialize the gulp pug task

---
+++ gulp/pug.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import fs from 'fs';
+import path from 'path';
+// foldero - will load files from the folder you specify and construct an object
+// with properties corresponding to each loaded file
+import foldero from 'foldero';
+
+const pug = ({
+  gulp
+}) => {
+  gulp.task('pug', () => {
+  });
+};
+
+export default pug;
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,8 +1,16 @@
 'use strict';
 
-// Gulp is a simple platform-agnostic toolkit that helps you automate painful
+// GUlp is a simple platform-agnostic toolkit that helps you automate painful
 // and time-consuming tasks in your workflow
 import gulp from 'gulp';
+import fs from 'fs';
+import wrench from 'wrench';
+
+// Read all files from the gulp folder and load all gulp tasks
+wrench.readdirSyncRecursive('./gulp')
+  // Filter the file collection to only allow JavaScript files
+  .filter(fileName => /\.(js)$/i.test(fileName))
+  .map(fileName => require(`./gulp/${fileName}`)({gulp}));
 
 // Default gulp task
 gulp.task('default', () => {
