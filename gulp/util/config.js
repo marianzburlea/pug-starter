e20badf2d998e6c158627ea44a018d0a149e5bc7
Marian Zburlea <marianzburlea@gmail.com>
2017-04-10 21:59:31 +0100

    Add pdf to the copy gulp process

--- gulp/util/config.js
+++ gulp/util/config.js
@@ -1,6 +1,6 @@
 const config = {
   fileExpression: {
-    copy: '**/*.{doc,docx,zip}',
+    copy: '**/*.{doc,docx,zip,pdf}',
     font: '**/*.{otf,eot,svg,ttf,woff,woff2}',
     image: '**/*.{jpg,jpeg,gif,svg,png}'
   }
