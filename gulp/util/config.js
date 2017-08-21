52f1d1dda60ba0bb1958490162a98391579e5744
Marian Zburlea <marianzburlea@gmail.com>
2017-08-21 12:52:59 +0100

    Add JavaScript to be copied as asset

--- gulp/util/config.js
+++ gulp/util/config.js
@@ -1,6 +1,6 @@
 const config = {
   fileExpression: {
-    copy: '**/*.{doc,docx,zip,pdf}',
+    copy: '**/*.{doc,docx,zip,pdf,js}',
     font: '**/*.{otf,eot,svg,ttf,woff,woff2}',
     image: '**/*.{jpg,jpeg,gif,svg,png}'
   }
