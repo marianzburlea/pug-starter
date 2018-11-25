c77d5bdfa1bd66f17d3377486ed52f7c90a6206c
Marian Zburlea <marianzburlea@gmail.com>
2018-11-25 19:23:22 +0000

    Add copy for first level child descendant ico and json files.

--- gulp/util/config.js
+++ gulp/util/config.js
@@ -3,7 +3,7 @@ const config = {
     copy: '**/*.{doc,docx,zip,pdf,js}',
     font: '**/*.{otf,eot,svg,ttf,woff,woff2}',
     image: '**/*.{jpg,jpeg,gif,svg,png}',
-    ico: '**/*.ico'
+    ico: './*.{ico,json}'
   }
 };
 
