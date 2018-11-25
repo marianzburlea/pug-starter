da92be7bf3e20478e84bf7a660d10e39cd9c4e29
Marian Zburlea <marianzburlea@gmail.com>
2018-11-25 19:12:57 +0000

    add ico property to fileExpression in config.js

--- gulp/util/config.js
+++ gulp/util/config.js
@@ -2,7 +2,8 @@ const config = {
   fileExpression: {
     copy: '**/*.{doc,docx,zip,pdf,js}',
     font: '**/*.{otf,eot,svg,ttf,woff,woff2}',
-    image: '**/*.{jpg,jpeg,gif,svg,png}'
+    image: '**/*.{jpg,jpeg,gif,svg,png}',
+    ico: '**/*.ico'
   }
 };
 
