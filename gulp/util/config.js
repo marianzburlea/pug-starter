d378bf6a1a288ad7b9f3e8b6ee346af44e4a542f
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2018-12-08 20:43:53 +0000

    Let's add a new file type in copy: mov and mp4

--- gulp/util/config.js
+++ gulp/util/config.js
@@ -1,6 +1,6 @@
 const config = {
   fileExpression: {
-    copy: '**/*.{doc,docx,zip,pdf,js}',
+    copy: '**/*.{doc,docx,zip,pdf,js,mov,mp4}',
     font: '**/*.{otf,eot,svg,ttf,woff,woff2}',
     image: '**/*.{jpg,jpeg,gif,svg,png}',
     ico: './*.{ico,json}'
