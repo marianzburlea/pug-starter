cb8b18e429ec3b7c38100485718fd4d780d8c8a5
Marian Zburlea <marian@wowdop.com>
2022-07-30 22:18:36 +0100

    Update 2022-07-30T21:18:32.642Z

---
+++ asset/js/all.js
@@ -0,0 +1,4 @@
+document.querySelector(".title__name--last").textContent =
+  "Mega Developer uuu! :) :)";
+
+console.log("You are going to become a rockstar web developer!");
---
+++ bundle.js
@@ -0,0 +1,8 @@
+(function () {
+  'use strict';
+
+  document.querySelector(".title__name--last").textContent = "Mega Developer uuu! :) :)";
+  console.log("You are going to become a rockstar web developer!");
+
+}());
+//# sourceMappingURL=bundle.js.map
